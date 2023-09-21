9f146f872a8e7b4cbb720888ada53e1be2fccae9
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2023-09-21 16:01:29 +0300

    new changes

--- js/main.js
+++ js/main.js
@@ -24,7 +24,40 @@ $(document).ready(function () {
         $('.categories__tab').removeClass('active');
         $(this).addClass('active');
         $('.' + dataClass).addClass('active-window').fadeIn(500);
+        if(dataClass == 'all') {
+            $('.categories__window').addClass('active-window').fadeIn(500);
+        }
         return false;
     });
+
+
+
+    // POST NAV SCRIPT --------------------- //
+    $(".postnav ul").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top - 105 }, 1500);
+    });
+
+    const links = document.querySelectorAll('.postnav li a');
+    window.addEventListener('scroll', () => {
+        const anchors = document.querySelectorAll('article [id]');
+        anchors.forEach(anchor => {
+            if (anchor.closest('article')) {
+                const anchorTop = anchor.getBoundingClientRect().top + window.pageYOffset;
+                if (window.pageYOffset >= anchorTop - 110) {
+                    links.forEach(link => {
+                        const correspondingLink = document.querySelector(`.postnav li a[href="#${anchor.id}"]`);
+                        if (correspondingLink) {
+                            link.classList.remove('active');
+                            correspondingLink.classList.add('active');
+                        }
+                    });
+                }
+            }
+        });
+    });
+
 
 });
